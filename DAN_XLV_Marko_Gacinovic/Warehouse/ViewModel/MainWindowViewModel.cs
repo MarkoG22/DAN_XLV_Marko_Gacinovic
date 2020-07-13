fb31372c14eaad1ac45d6909a12a096f0ac9ce08
MarkoG22 <marko.gacinovic.el@gmail.com>
2020-07-13 17:45:29 +0200

    MainWindow View and ViewModel

---
+++ DAN_XLV_Marko_Gacinovic/Warehouse/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Warehouse.Commands;
+using Warehouse.View;
+
+namespace Warehouse.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        MainWindow main;
+
+        // properties for username and password
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+        }
+
+        // command for the login button
+        private ICommand logIn;
+        public ICommand LogIn
+        {
+            get
+            {
+                if (logIn == null)
+                {
+                    logIn = new RelayCommand(param => SaveExecute(), param => CanSaveExecute());
+                }
+                return logIn;
+            }
+        }
+
+        private bool CanSaveExecute()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// method for checking username and password and opening the windows
+        /// </summary>
+        private void SaveExecute()
+        {
+            if (username == "Mag2019" && password == "Mag2019")
+            {
+                StorekeeperView storekeeper = new StorekeeperView();
+                storekeeper.ShowDialog();
+            }
+            else if (username == "Man2019" && password == "Man2019")
+            {
+                ManagerView manager = new ManagerView();
+                manager.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Wrong username or password, please try again.");
+            }
+        }
+
+        // command for closing the window
+        private ICommand logout;
+        public ICommand Logout
+        {
+            get
+            {
+                if (logout == null)
+                {
+                    logout = new RelayCommand(param => CloseExecute(), param => CanCloseExecute());
+                }
+                return logout;
+            }
+        }
+
+        /// <summary>
+        /// method for closing the window
+        /// </summary>
+        private void CloseExecute()
+        {
+            try
+            {
+                main.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception" + ex.Message.ToString());
+            }
+        }
+
+        private bool CanCloseExecute()
+        {
+            return true;
+        }
+    }
+}
